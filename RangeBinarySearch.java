0b600cc96a4f619c8718b8ba6372038338d0623a
aminmah <aminmah@student.chalmers.se>
2022-02-02 12:25:33 +0100

    RangeBinarySearch commitment.

--- RangeBinarySearch.java
+++ RangeBinarySearch.java
@@ -9,7 +9,34 @@ public class RangeBinarySearch {
     // Complexity: O(log N) comparisons where N is the length of `a`
     public static<T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-        throw new UnsupportedOperationException();
+        int lo=0;
+        int hi= a.length-1;
+        int answer=0;
+
+        while (lo<hi){
+            int mid= (lo+hi)/2;
+            int result = comparator.compare(a[mid],key);
+            if( result == -1 ) {
+                lo= mid+1;
+            } else if (result == 1) {
+                hi = mid-1;
+            } else if (result == 0) {
+                hi= mid;
+                if(lo<hi) {
+                    if( result == 0 ) {
+                        hi = mid;
+                    } else if (result == 1) {
+                        lo= mid+1;
+                    }
+                } else if (lo==hi) {
+                    answer =mid;
+                }
+            } else {
+                return -1;
+            }
+        }
+
+        return answer;
     }
 
     // Returns the index of the *last* element in `a` that equals the search key,
@@ -18,12 +45,57 @@ public class RangeBinarySearch {
     // Complexity: O(log N) comparisons where N is the length of `a`
     public static<T> int lastIndexOf(T[] a, T key, Comparator<T> comparator) {
         // TODO
-        throw new UnsupportedOperationException();
+        int lo=0;
+        int hi= a.length-1;
+        int answer=0;
+
+        while (lo<hi){
+            int mid= (lo+hi)/2;
+            int result = comparator.compare(a[mid],key);
+            if( result == -1 ) {
+                lo= mid+1;
+            } else if (result == 1) {
+                hi = mid-1;
+            } else if (result == 0) {
+                hi= mid;
+                if(lo<hi) {
+                    if( result == 0 ) {
+                        hi = mid;
+                    } else if (result == 1) {
+                        lo= mid+1;
+                    }
+                } else if (lo==hi) {
+                    answer =mid;
+                }
+
+            } else {
+                return -1;
+            }
+        }
+
+        return answer;
     }
 
+
+    public static final Comparator<String> alphabetically = new Comparator<String>() {
+        @Override
+
+        public int compare(String o1, String o2) {
+            return o1.compareToIgnoreCase(o2);
+        }
+    } ;
 
     // For testing purposes.
     public static void main(String[] args) {
         // Here you can write some tests if you want.
+
+
+
+        String[] T = {"a","a","b","b","c","c","c"};
+        int answer= firstIndexOf(T,"a", alphabetically);
+        System.out.println(answer);
+
+        int secondAnswer= lastIndexOf(T, "c", alphabetically);
+        System.out.println(secondAnswer);
     }
 }
